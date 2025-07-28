b53271bd5302da3682022627fb13816f0c734c5a
Wahid Patel <patel.wahid2005@gmail.com>
2025-07-28 19:59:40 +0530

    final weather app

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
+
 // Weather App Script`
+document.addEventListener('DOMContentLoaded', () => {
+    let cityInput = document.getElementById("city-input")
+    const getWeather = document.getElementById("get-weather-btn")
+    const weatherInfo = document.getElementById("weather-info")
+    const cityNameDisplay = document.getElementById("city-name")
+    const tempDisplay = document.getElementById("temperature")
+    const descriptionDisplay = document.getElementById("description")
+    const errorMessage = document.getElementById("error-message")
+    
+    
+
+    getWeather.addEventListener('click', async () => {
+        const city = cityInput.value.trim()
+        if(!city) return;
+        //it may throw an error
+        //server or database is always in another continent
+
+        try {
+            const weatherData = await fetchWeatherData(city)
+            displayWeatherData(weatherData)
+            cityInput.value = ""
+        } catch (error) {
+            displayError()
+            cityInput.value = ""
+        }
+    })
+
+    async function fetchWeatherData (city){
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${API_KEY}`
+
+        const response = await fetch(url);
+        console.log(response);
+        
+        if(!response.ok){
+            throw new Error("City not found");
+            
+        }
+        data = await response.json()
+        return data
+
+    }
+    function displayWeatherData(data){
+        weatherInfo.classList.remove('hidden')
+        errorMessage.classList.add("hidden")
+
+        const {name, main, weather} = data;
+        cityNameDisplay.textContent = `City: ${name}`
+        tempDisplay.textContent = `Temperature: ${main.temp}`
+        descriptionDisplay.textContent = `Description: ${weather[0].description}`
+
+    }
+    function displayError (){
+        weatherInfo.classList.add('hidden')
+        errorMessage.classList.remove("hidden")
+    }
+
+})
